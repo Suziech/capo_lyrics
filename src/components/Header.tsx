ee7e8d748fd25cdeb7157a262a314880042fbb49
최슬기 <choisk09@gmail.com>
2024-12-08 03:00:33 +0900

    chore: add logo, contents, and set header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,27 +5,23 @@ import type { LocaleTypes } from "@/utils/localization/settings";
 import Link from "next/link";
 import { useTranslation } from "@/utils/localization/client";
 import ChangeLocale from "./ChangeLocale";
-import ThemeChanger from "./ThemeChanger";
+import logo from "@/assets/images/logo_image.png";
+import Image from "next/image";
+// import ThemeChanger from "./ThemeChanger";
 
 export default function Header() {
   const pathName = usePathname();
   const locale = useParams()?.locale as LocaleTypes;
   const { t } = useTranslation(locale, "common");
   return (
-    <header className={`flex`}>
+    <header
+      className={`flex items-center max-w-[1440px] mx-auto py-[20px] px-[16px]`}>
       <nav className={`flex justify-between w-full dark:text-red-500`}>
         <Link href={`/${locale}`}>
-          <b>GingaTunes 🤸‍♀️</b>
+          {/* <b className='text-2xl'>Capo-Lyrics 🤸‍♀️</b> */}
+          <Image src={logo} alt='logo' width={160} height={30} />
         </Link>
-        <div>
-          {" "}
-          <Link
-            href={`/${locale}`}
-            className={
-              pathName === `/${locale}` || pathName === "/" ? "selected" : ""
-            }>
-            {t("home")}
-          </Link>
+        <div className='flex items-center px-[10px]'>
           <Link
             href={`/${locale}/about`}
             className={
@@ -33,12 +29,12 @@ export default function Header() {
                 ? "selected"
                 : ""
             }>
-            {t("about")}
+            About
           </Link>
         </div>
       </nav>
       <ChangeLocale />
-      <ThemeChanger />
+      {/* <ThemeChanger /> */}
     </header>
   );
 }
