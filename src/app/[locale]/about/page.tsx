b6ed728f750537e7fe2d92da7e0e39a78acd46bb
최슬기 <choisk09@gmail.com>
2024-12-08 03:02:18 +0900

    chore: add japanese version

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -7,5 +7,20 @@ export default async function Page({
   params: { locale: LocaleTypes };
 }) {
   const { t } = await createTranslation(locale, "about");
-  return <div>{t("about")}</div>;
+  return (
+    <div className='px-[20px] flex flex-col items-center'>
+      <h1 className='text-2xl font-bold p-6 text-center whitespace-pre-wrap'>
+        {t("title")}
+      </h1>
+      <p className='text-sm whitespace-pre-wrap leading-6'>
+        {t("description")}
+      </p>
+
+      <button className='bg-blue-500 text-white px-4 py-2 rounded-md my-10 hover:bg-blue-600'>
+        <a href='mailto:ccsk09@naver.com' className='block text-white'>
+          {t("sendSong")}
+        </a>
+      </button>
+    </div>
+  );
 }
--- src/components/Lyrics.tsx
+++ src/components/Lyrics.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import songs from "@/data/songs";
+import songs from "@/data/songs.json";
 
 export default function Lyrics({
   slug,
@@ -14,20 +14,21 @@ export default function Lyrics({
   }
   const song = songs.find((item) => item.slug === slug);
   return (
-    <div className='container mx-auto p-4'>
+    <div className='container mx-auto p-4 flex flex-col justify-center items-center'>
       <iframe
         src={song?.youtubeLink}
         title='YouTube video player'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
         referrerPolicy='strict-origin-when-cross-origin'
         allowFullScreen></iframe>
-      <h1 className='text-2xl font-bold'>{song?.title}</h1>
+      <h1 className='text-2xl font-bold py-5'>{song?.title}</h1>
       <ul>
         {song?.lyrics.map((item) => (
-          <li key={item.original}>
+          <li key={item.id}>
             <p>{item.original}</p>
-            {locale === "en" && <p>{item.en}</p>}
-            {locale === "ko" && <p>{item.ko}</p>}
+            {locale === "en" && <p className='text-gray-500'>{item.en}</p>}
+            {locale === "ko" && <p className='text-gray-500'>{item.ko}</p>}
+            {locale === "ja" && <p className='text-gray-500'>{item.ja}</p>}
           </li>
         ))}
       </ul>
