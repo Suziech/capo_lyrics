2b471628018d59cd2972688e415a1852f8ef1c10
최슬기 <choisk09@gmail.com>
2024-12-08 21:22:13 +0900

    chore: add close function modal of language selection by clicking outside

--- src/components/ChangeLocale.tsx
+++ src/components/ChangeLocale.tsx
@@ -6,28 +6,47 @@ import {
   useSelectedLayoutSegments,
 } from "next/navigation";
 import { IoLanguage } from "react-icons/io5";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function ChangeLocale() {
   const router = useRouter();
   const params = useParams();
   const urlSegments = useSelectedLayoutSegments();
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleLocaleChange = (newLocale: string) => {
     const updatedSegments = urlSegments.filter(
       (segment) => segment !== params.locale
     );
     router.push(`/${newLocale}/${updatedSegments.join("/")}`);
+    setIsOpen(false); // 메뉴 선택 후 닫기
   };
 
+  // Outside click handler
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div
-      className='px-4 relative cursor-pointer'
-      onClick={() => setIsOpen(!isOpen)}>
-      <IoLanguage size={24} />
+    <div className='px-4 relative cursor-pointer' ref={dropdownRef}>
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <IoLanguage size={24} />
+      </div>
       {isOpen && (
-        <div className='absolute top-10 right-0 bg-white border rounded-md w-[110px] text-black p-1'>
+        <div className='absolute top-10 right-0 bg-white border rounded-md w-[110px] text-black p-1 z-10'>
           <div onClick={() => handleLocaleChange("en")} className='p-2'>
             🇬🇧 English
           </div>
