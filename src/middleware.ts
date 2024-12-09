0f30311939bdb7da93519cdc60e0da35a9d69edf
최슬기 <choisk09@gmail.com>
2024-12-10 00:07:25 +0900

    chore: edit middleware to get the language from the device as a default

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,38 @@
 // middleware.ts
 import { NextResponse, NextRequest } from "next/server";
-import { fallbackLng, locales } from "@/utils/localization/settings";
+import {
+  fallbackLng,
+  locales,
+  LocaleTypes,
+} from "@/utils/localization/settings";
 
 export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
 
-  // Check if the default locale is in the pathname
-  if (
-    pathname.startsWith(`/${fallbackLng}/`) ||
-    pathname === `/${fallbackLng}`
-  ) {
-    // e.g. incoming request is /en/about
-    // The new URL is now /about
-    return NextResponse.redirect(
-      new URL(
-        pathname.replace(
-          `/${fallbackLng}`,
-          pathname === `/${fallbackLng}` ? "/" : ""
-        ),
-        request.url
-      )
-    );
-  }
+  // Extract user language from request headers
+  const acceptLanguage = request.headers.get("accept-language") || "";
+  const userLanguage = acceptLanguage.split(",")[0].split("-")[0]; // Get the base language, e.g., "en" from "en-US"
 
+  // Determine the locale to use
+  const detectedLocale = locales.includes(userLanguage as LocaleTypes)
+    ? (userLanguage as LocaleTypes)
+    : fallbackLng;
+
+  // Redirect if locale is missing in the pathname
   const pathnameIsMissingLocale = locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
   if (pathnameIsMissingLocale) {
-    // We are on the default locale
-    // Rewrite so Next.js understands
-
-    // e.g. incoming request is /about
-    // Tell Next.js it should pretend it's /en/about
-    return NextResponse.rewrite(
-      new URL(`/${fallbackLng}${pathname}`, request.url)
+    return NextResponse.redirect(
+      new URL(`/${detectedLocale}${pathname}`, request.url)
     );
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  // Do not run the middleware on the following paths
   matcher: [
     "/((?!api|.*\\..*|_next/static|_next/image|manifest.json|assets|favicon.ico).*)",
   ],
