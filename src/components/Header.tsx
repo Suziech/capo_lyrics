26fa6f59e40410587bc2bace466150c9b94f02f3
최슬기 <choisk09@gmail.com>
2024-12-13 20:32:36 +0900

    chore: adjust header position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,26 +14,29 @@ export default function Header() {
   const locale = useParams()?.locale as LocaleTypes;
   // const { t } = useTranslation(locale, "common");
   return (
-    <header
-      className={`flex items-center max-w-[1440px] mx-auto py-[20px] px-[16px] fixed w-full bg-[#FFC436] z-[9999]`}>
-      <nav className={`flex justify-between w-full `}>
-        <Link href={`/${locale}`}>
-          <Image src={logo} alt='logo' width={160} height={30} />
-        </Link>
-        <div className='flex items-center px-[10px] text-[#00712D]'>
-          <Link
-            href={`/${locale}/about`}
-            className={
-              pathName === `/${locale}/about` || pathName === "/about"
-                ? "selected font-bold"
-                : ""
-            }>
-            About
+    <div className='w-full flex justify-center'>
+      {" "}
+      <header
+        className={`flex items-center max-w-[1440px] mx-auto py-[20px] px-[16px] fixed w-full bg-[#FFC436] z-[9999]`}>
+        <nav className={`flex justify-between w-full `}>
+          <Link href={`/${locale}`}>
+            <Image src={logo} alt='logo' width={160} height={30} />
           </Link>
-        </div>
-      </nav>
-      <ChangeLocale />
-      {/* <ThemeChanger /> */}
-    </header>
+          <div className='flex items-center px-[10px] text-[#00712D]'>
+            <Link
+              href={`/${locale}/about`}
+              className={
+                pathName === `/${locale}/about` || pathName === "/about"
+                  ? "selected font-bold"
+                  : ""
+              }>
+              About
+            </Link>
+          </div>
+        </nav>
+        <ChangeLocale />
+        {/* <ThemeChanger /> */}
+      </header>
+    </div>
   );
 }
