7b122adcc088b57626cd21ce28e9472085c10935
최슬기 <choisk09@gmail.com>
2024-12-13 21:40:50 +0900

    chore: add img to the list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['img.youtube.com'], // 외부 도메인 추가
+    },
+};
 
 export default nextConfig;
