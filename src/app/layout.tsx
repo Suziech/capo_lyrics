f7f8806f830934c60cbc43a066fc29cf502f7af6
최슬기 <choisk09@gmail.com>
2024-12-16 22:05:06 +0900

    feat: add google adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
 
   return (
     <html lang='en'>
+      <head>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2001340164243542'
+          crossOrigin='anonymous'></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased ${theme}`}>
         {children}
