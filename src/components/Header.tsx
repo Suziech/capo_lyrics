4eccae85ebec9b8d0729766bc31a557f295575ef
최슬기 <choisk09@gmail.com>
2025-01-20 21:51:10 +0900

    feat: add scroll effect to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { useParams, usePathname } from "next/navigation";
 import type { LocaleTypes } from "@/utils/localization/settings";
 import Link from "next/link";
@@ -13,8 +14,35 @@ export default function Header() {
   const pathName = usePathname();
   const locale = useParams()?.locale as LocaleTypes;
   // const { t } = useTranslation(locale, "common");
+
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        setShowHeader(false); // 스크롤을 내릴 때 헤더 숨김
+      } else {
+        setShowHeader(true); // 스크롤을 올릴 때 헤더 표시
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <div className='w-full flex justify-center'>
+    <div
+      className={`w-full flex justify-center ${
+        showHeader ? "top-0" : "-top-[80px]"
+      } transition-all duration-300 ease-in-out fixed z-[9999]`}>
       {" "}
       <header
         className={`flex items-center max-w-[1440px] mx-auto py-[20px] px-[16px] fixed w-full bg-[#000] z-[9999]`}>
